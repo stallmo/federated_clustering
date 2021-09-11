c0ec9d5dd3b17e960e161ded9991729e50e324f4
stallmo <morris.stallmann@gmail.com>
2021-09-11 10:43:50 +0200

    add kmeans weighing

--- global_learner.py
+++ global_learner.py
@@ -1,16 +1,18 @@
 import numpy as np
 import copy
-
+from sklearn.cluster import KMeans
 
 class GlobalClusterer:
 
-    def __init__(self, local_learners, num_clusters, data_dim, max_rounds=1000, tol=0.0001, weighing_function=None):
+    def __init__(self, local_learners, num_clusters, data_dim, max_rounds=1000, tol=0.0001,
+                 global_center_update='fedavg', weighing_function=None):
         self.__local_learners = local_learners
         self.__tol = tol
         self.__num_clusters = num_clusters
         self.__max_rounds = max_rounds
         self.__data_dim = data_dim
         self.__weighing_function = weighing_function
+        self.__global_center_update = global_center_update  # method applied to update global cluster centers
 
         # going to be set when fit() is called
         self.__W = None  # clients cluster supports
@@ -49,6 +51,44 @@ class GlobalClusterer:
     def iterations(self):
         return self.__iterations
 
+    def __fedavg_update(self, local_center_supports, new_centers):
+        """
+        Method that applied federated averaging to update the global cluster centers.
+
+        :param local_center_supports: List of np.arrays. Each array has <num_cluster> entries, i.e. the support for each cluster.
+        :param new_centers:list of np.arrays. Each array has <num_cluster> rows and <data_dimension> columns.
+        :return: None
+        """
+        support_normalizer = np.array(local_center_supports).sum(axis=0)
+        new_centers = np.array(new_centers)
+        # multiple each client's center with its support
+        weighted_new_centers = np.array(new_centers).reshape(-1, self.__data_dim) * local_center_supports.reshape(-1, 1)
+        # reshape to prepare for normalizing by total support
+        weighted_new_centers = weighted_new_centers.reshape(-1, self.__num_clusters, self.__data_dim)
+        # calculate new global centers
+        self.__global_centers = weighted_new_centers.sum(axis=0) / support_normalizer.reshape(-1, 1)
+
+    def __kmeans_center_update(self, new_centers, local_center_supports):
+        """
+        Method that applies kmeans to the
+
+        :param new_centers:
+        :param local_center_supports:
+        :return:
+        """
+        kmeans_model = KMeans(n_clusters=self.__num_clusters,
+                              init='k-means++',
+                              max_iter=500,
+                              precompute_distances='auto',
+                              random_state=43,
+                              n_jobs=None)
+
+        new_centers = np.array(new_centers).reshape(-1, self.__data_dim)
+        kmeans_model.fit(new_centers)
+        self.__global_centers = kmeans_model.cluster_centers_
+
+        pass
+
     def __update_global_centers(self, new_centers, local_center_supports):
         """
         Update routine that calculates new centers from each clients information.
@@ -62,14 +102,10 @@ class GlobalClusterer:
         if not self.__weighing_function is None:
             local_center_supports = self.__weighing_function(local_center_supports)
 
-        support_normalizer = np.array(local_center_supports).sum(axis=0)
-        new_centers = np.array(new_centers)
-        # multiple each client's center with its support
-        weighted_new_centers = np.array(new_centers).reshape(-1, self.__data_dim) * local_center_supports.reshape(-1, 1)
-        # reshape to prepare for normalizing by total support
-        weighted_new_centers = weighted_new_centers.reshape(-1, self.__num_clusters, self.__data_dim)
-        # calculate new global centers
-        self.__global_centers = weighted_new_centers.sum(axis=0) / support_normalizer.reshape(-1, 1)
+        if self.__global_center_update == 'fedavg':
+            self.__fedavg_update(local_center_supports, new_centers)
+        elif self.__global_center_update == 'kmeans':
+            self.__kmeans_center_update(new_centers, local_center_supports)
 
     def fit(self):
         """
@@ -105,4 +141,5 @@ class GlobalClusterer:
                 for local_learner in self.__local_learners:
                     local_learner.set_centers(self.__global_centers)
                 break
+
         pass
